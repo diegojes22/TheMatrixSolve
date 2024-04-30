6d376dceff32c89ae23f9a46df6ebd80dba9ac71
Diego Muñoz <diego22.tec@gmail.com>
2024-04-29 18:08:21 -0600

    Implementacion del primer caso practico

---
+++ CreatePolinomium.java
@@ -0,0 +1,143 @@
+package net.diego.sistemasdeecuaciones;
+
+/**
+ *
+ * @author Diego Muñoz
+ */
+public class CreatePolinomium {
+    /* Atributos */
+    private int n, m;
+    private int grado = 0;
+    private double[][] points;
+    private Matriz matriz;
+    
+    /* Metodos */
+    public CreatePolinomium(double points[][]) {
+        setPoints(points);
+    }
+    
+    public void setPoints(double points[][]) {
+        this.points = points;
+        
+        this.n = points.length;
+        this.m = points.length+1;
+        
+        grado = n-1;
+        
+        matriz = new Matriz(n, m);
+    }
+    
+    /**
+     * Establesemos la matriz en base a los puntos
+     * dados anteriormente.
+     */
+    public void setMatriz() {
+        // Rellenar matriz de cofactores
+        for(int j = 0; j < m-1; j++) {
+            for(int i = 0; i < n; i++) {
+                matriz.updateMatrizVal(i, j, Math.pow(points[i][0], j));
+            }
+        }
+        
+        // Insertar terminos independientes
+        for(int i = 0; i < n; i++) {
+            matriz.updateMatrizVal(i, m-1, points[i][1]);
+        }
+    }
+    
+    /**
+     * Obtenemos la matriz resultante de la operacion
+     * anteriormente realizada.
+     * @return 
+     */
+    public Matriz getMatrizSystem() {
+        return matriz;
+    }
+    
+    /**
+     * Metodo estatico para generar una matriz en base a 
+     * n puntos.
+     * 
+     * @param points
+     * @return 
+     */
+    public static Matriz getSystem(double[][] points) {
+        CreatePolinomium p = new CreatePolinomium(points);
+        p.setMatriz();
+        return p.getMatrizSystem();
+    }
+    
+    /**
+     * Crear el polinomio en base a los resultados
+     * de las variables.
+     * 
+     * @param vals
+     * @return 
+     */
+    public static String getPolinomium(double[] vals) {
+        String px = "";
+        
+        int i = 0;
+        for(i = 0; i < vals.length; i++) {
+            if(i == 0) {                  // potencia igual a 0
+                px += vals[i]+" + ";
+            }
+            else if(i == 1) {             // potencia igual a 1
+                px += vals[i]+"*x "+" + ";
+            }
+            else if(i == vals.length-1) { // ultimo valor del polinomio
+                px += vals[i]+"*x**"+i;
+            }
+            else {                        // otro caso
+                px += vals[i]+"*x**"+i+" + ";
+            }
+        }
+        
+        if(i == 2) {
+            px = px.substring(0, px.length() - 1);
+            px = px.substring(0, px.length() - 1);
+        }
+        
+        return px;
+    }
+    
+    /**
+     * Graficamos un polinomio con un script externo
+     * a partir de un STring generado por los
+     * procedimientos de la clase.
+     * 
+     * @param function 
+     */
+    public static void grapichs(String function) {
+        try {
+            // Invocar script para graficar
+            String command = "python D:\\ProgramingProyects\\Python\\graficadora\\main.py \""+function+"\"";
+            Process child = Runtime.getRuntime().exec(command);
+        }
+        catch (Exception errno) {
+            System.out.println("Error desconocido: "+errno.getMessage());
+        }
+    }
+    
+    /* TestZone */
+    /*public static void main(String[] args) {
+        // puntos
+        double[][] puntos = {
+            {-2, 12},
+            {0, 0},
+            {2, 12}
+        };
+        
+        // obtener sistema de ecuaciones
+        Matriz m = CreatePolinomium.getSystem(puntos);
+        
+        // resolver y obtener polinomio
+        double[] r = MetodoGaussJordan.solve(m);
+        String px = CreatePolinomium.getPolinomium(r);
+        
+        System.out.println(m.printMatriz());
+        System.out.println(px);
+        
+        CreatePolinomium.grapichs(px);
+    }*/
+}
---
+++ PolinomiumMatrix.java
@@ -0,0 +1,343 @@
+package net.diego.sistemasdeecuaciones;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+ *
+ * @author Diego Muñoz
+ */
+public class PolinomiumMatrix extends JFrame implements ActionListener, ItemListener{
+    /* Atributos */
+    private int grade;
+    private boolean pointsExists = false;
+    private double[][] points;
+    private boolean inProcess = false;
+    
+    private Matriz matriz;
+    private String fx;
+    
+    // Widgets
+    JLabel tagForXInput;
+    JLabel tagForYInput;
+    JTextField inputs[][];
+    
+    JButton configPoints;
+    JButton clearPoints;
+    
+    JComboBox methods;
+    
+    JLabel[][] outSystemMatriz;
+    
+    /* Metodos */
+    // Constructor
+    public PolinomiumMatrix() {
+        configWin();
+        
+        initWidgets();
+        configWidgets();
+        buttonEvents();
+        addWidgets();
+    }
+    
+    /**
+     * Configurar aspectos basicos de la ventana, como
+     * el tamaño, titulo, etc.
+     */
+    private void configWin() {
+        this.setLayout(null);
+        this.setBounds(10, 10, 500, 400);
+        this.setVisible(true);
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.repaint();
+    }
+    
+    /**
+     * Inicializar todos los widgets
+     */
+    private void initWidgets() {
+        tagForXInput = new JLabel("X");
+        tagForYInput = new JLabel("Y");
+        
+        configPoints = new JButton("Nuevo");
+        clearPoints = new JButton("Eliminar");
+        
+        methods = new JComboBox();
+    }
+    
+    /**
+     * Ajustar y estilizar los widgets
+     */
+    private void configWidgets() {
+        tagForXInput.setBounds(10, 0, 90, 20);
+        tagForYInput.setBounds(110, 0, 90, 20);
+        
+        configPoints.setBounds(10, 270, 90, 20);
+        clearPoints.setBounds(100, 270, 90, 20);
+        
+        methods.setBounds(10, 300, 190, 20);
+    }
+    
+    /**
+     * Agregar funcionalidad a los botones
+     */
+    private void buttonEvents() {
+        configPoints.addActionListener(this);
+        clearPoints.addActionListener(this);
+        
+        methods.addItem("seleccionar...");
+        methods.addItem("Gauss");
+        methods.addItem("Gauss-Jordan");
+        methods.addItem("Cramer");
+        methods.addItemListener(this);
+    }
+    
+    /**
+     * Agregar widgets en la ventana
+     */
+    private void addWidgets() {
+        add(tagForXInput);
+        add(tagForYInput);
+        
+        add(configPoints);
+        add(clearPoints);
+        
+        add(methods);
+    }
+    
+    /**
+     * Crear inputs dinamicamente
+     */
+    private void createInputs() {
+        int coordX = 10;
+        int coordY = 30;
+        
+        // solicitar cuantos inputs
+        if(getPolinomiumGrade()) {
+            inputs = new JTextField[grade][2];
+            points = new double[grade][2];
+            
+            for(int x = 0; x < 2; x++) {
+                for(int y = 0; y < grade; y++) {
+                    inputs[y][x] = new JTextField();
+                    inputs[y][x].setBounds(coordX, coordY, 90, 20);
+                    add(inputs[y][x]);
+                    
+                    coordY += 20;
+                }
+                
+                coordY = 30;
+                coordX += 100;
+            }
+        }
+        
+        pointsExists = true;
+        this.repaint();
+    }
+    
+    /**
+     * Eliminamos los inputs y restablecemos los valores
+     * relacionados para evitar conflictos.
+     */
+    private void deleteInputs() {
+        for(int x = 0; x < 2; x++) {
+            for(int y = 0; y < grade; y++) {
+                this.remove(inputs[y][x]);
+            }
+        }
+        
+        inputs = null;
+        grade = 0;
+        matriz = null;
+        points = null;
+        pointsExists = false;
+        
+        deleteOutSystem();
+        this.repaint();
+    }
+    
+    /**
+     * Generar la matriz en base los puntos
+     */
+    private void buildMatrizSystem() {
+        // obtener valores
+        for(int x = 0; x < 2; x++) {
+            for(int y = 0; y < grade; y++) {
+                points[y][x] = Double.parseDouble(inputs[y][x].getText());
+            }
+        }
+        
+        matriz = CreatePolinomium.getSystem(points);
+    }
+    
+    /**
+     * Obtener el grado del polinomio para poder definir cuantos
+     * inputs se nesecitan.
+     * 
+     * @return 
+     */
+    private boolean getPolinomiumGrade() {
+        try {
+            grade = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de puntos"));
+            return true;
+        }
+        catch(Exception errno) {
+            errorMessage("Error: "+errno.getMessage(), "Error al leer datos");
+        }
+        return false;
+    }
+    
+    /**
+     * Algunas verificaciones para la matriz.
+     * @return 
+     */
+    private boolean checkMatriz() {
+        if(matriz == null) {
+            errorMessage("Error de matriz", "La matriz esta vacia");
+            return false;
+        }
+        
+        return true;
+    }   
+    
+    /**
+     * Crear etiquetas de salida para el sistema de ecuaciones
+     */
+    private void createOutSystem() {
+        int coordX = 210;
+        int coordY = 10;
+        
+        try {
+            deleteOutSystem();
+        }catch(Exception e) {
+            
+        }
+        
+        outSystemMatriz = new JLabel[matriz.getN()][matriz.getM()];
+        
+        for(int n = 0; n < matriz.getN(); n++) {
+            for(int m = 0; m < matriz.getM(); m++) {
+                outSystemMatriz[n][m] = new JLabel(matriz.getMatrizVal(n, m)+"");
+                outSystemMatriz[n][m].setBounds(coordX, coordY, 50, 20);
+                this.add(outSystemMatriz[n][m]);
+                
+                coordY += 20;
+            }
+            
+            coordY = 10;
+            coordX += 50;
+        }
+        
+        this.repaint();
+    }
+    
+    /**
+     * eliminar etiquetas de salida.
+     */
+    private void deleteOutSystem() {
+        for(int n = 0; n < outSystemMatriz.length; n++) {
+            for(int m = 0; m < outSystemMatriz[0].length; m++) {
+                this.remove(outSystemMatriz[n][m]);
+            }
+        }
+        
+        outSystemMatriz = null;
+    }
+    
+    /**
+     * Resolver de acuerdo con el metodo seleccionado.
+     */
+    private void solve() {
+        String selection = methods.getSelectedItem().toString();
+        buildMatrizSystem();
+        
+        if(!checkMatriz()) return;
+            
+        // Resolver con Gauss
+        if(selection.equalsIgnoreCase("Gauss")) {
+            Gauss.solve(matriz);
+            
+            fx = CreatePolinomium.getPolinomium(MetodoGaussJordan.solve(matriz));
+        }
+        // Resolver con Gauss-Jordan
+        else if(selection.equalsIgnoreCase("Gauss-Jordan")) {
+            fx = CreatePolinomium.getPolinomium(MetodoGaussJordan.solve(matriz));
+        }
+        // Preguntar Cramer
+        else if(selection.equalsIgnoreCase("Cramer")) {
+            // dimensiones de la matriz
+            int n = matriz.getN();
+            int m = matriz.getM()-1;
+            
+            // obtener matriz de cofactores
+            Matriz temp = new Matriz(n, m);
+            double[] independientes = new double[m];
+            
+            // obtener los terminos independientes
+            for(int i = 0; i < n; i++) {
+                independientes[i] = matriz.getMatrizVal(i, m);
+            }
+            
+            // rellenar coeficientes
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < m; j++) {
+                    temp.updateMatrizVal(i, j, matriz.getMatrizVal(i, j));
+                }
+            }
+            
+           fx = CreatePolinomium.getPolinomium(MetodoCramer.solve(temp, independientes));
+        }
+        
+        
+        createOutSystem();
+        simpleMessage("Graficando", "Pulse OK para continuar..."); // freno para evitar mas subprocesos
+        CreatePolinomium.grapichs(fx); // graficar polinomio
+    }
+    
+    /**
+     * Simplificar el metodo para mostrar un dialogo de error.
+     * Muy util para las exepciones.
+     * 
+     * @param title
+     * @param text 
+     */
+    private void errorMessage(String title, String text) {
+        JOptionPane.showMessageDialog(this, text, title, JOptionPane.ERROR_MESSAGE);
+    }
+    
+    private void simpleMessage(String title, String text) {
+        JOptionPane.showMessageDialog(this, text, title, JOptionPane.NO_OPTION);
+    }
+    
+    // Eventos
+    
+    // Escuchar eventos
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == this.configPoints && !pointsExists) {
+            createInputs();
+        }
+        else if(e.getSource() == this.clearPoints && pointsExists) {
+            deleteInputs();
+        }
+    }
+    
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        if(e.getSource() == methods && pointsExists) {
+            if(!inProcess) {
+                solve();
+                inProcess = true;
+                return;
+            }
+            
+            inProcess = false;
+        }
+    }
+    
+    /* Test zone */
+    public static void main(String[] args) {
+        PolinomiumMatrix win = new PolinomiumMatrix();
+    }
+}
